16ab12cd14f19a855093cb5911f499c5718f25df
cbesmhsyn96 <system.c.hsyn@gmail.com>
2025-09-06 16:10:00 +0300

    modified:   sendEmail.sh

--- sendEmail.sh
+++ sendEmail.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-buildNumber="${BUILD_NUMBER}" 
-zipUrl="http://localhost:8080/job/EnuygunAppTestAutomation_Android/${buildNumber}/artifact/allure-report.zip"
+ZIP_PATH="${WORKSPACE}/allure-report.zip"
 
 source /Users/huseyinakcan/Desktop/sh_files/mail_env.sh && \
-curl -o allure-report.zip ${zipUrl} && \
-curl -url "smtp://smtp.gmail.com:465" \
+
+curl --url "smtps://smtp.gmail.com:465" \
 --ssl-reqd \
 --mail-from "${MAIL_FROM}" \
 --mail-rcpt "${MAIL_TO}" \
---upload-file allure-report.zip \
---upload-file emailBody.txt \
+--upload-file "${ZIP_PATH}" \
+--upload-file "emailBody.txt" \
 --user "${MAIL_FROM}:${APP_PASSWORD}"
